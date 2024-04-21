eaa474577e79d350e986d6037db9fb0e30af33e0
DimalJay <dimalkavindujay@gmail.com>
2024-04-21 10:15:00 +0530

    update main.py

--- bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/main.dart
+++ bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/main.dart
@@ -1,1 +1,10 @@
-// Hello {{name}}
+import 'package:flutter/material.dart';
+
+class {{name.pascalCase()}} extends StatelessWidget {
+  const {{name.pascalCase()}}({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
