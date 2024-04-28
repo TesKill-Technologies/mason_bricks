7eb3d8f0d52c252cfdef46e39812d332c8a07a23
DimalJay <dimalkavindujay@gmail.com>
2024-04-28 12:30:45 +0530

    update structure

---
+++ bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
@@ -0,0 +1,5 @@
+export 'domain/models/models.dart';
+export 'domain/providers/providers.dart';
+export 'presentation/screens/screens.dart';
+export 'presentation/view_models/view_models.dart';
+export 'presentation/widgets/widgets.dart';
